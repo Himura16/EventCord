c5c74b67f3ce70e63dfb2fdd0cf6be2cf331dc89
Himura16 <70828450+Himura16@users.noreply.github.com>
2020-11-03 02:50:52 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = (client) => { // runs when the bot is logged in
   console.log(`Logged in as ${client.user.tag}! `);
-  client.user.setActivity("Logging events... (+help)"); // sets the "game" activity
+  client.user.setActivity("e!help | EventCord"); // sets the "game" activity
   // client.db.run(`DELETE FROM calendar WHERE guild = ?`, ["473881982363762688"], (err) => {
   //   if (err) {
   //     console.error(err.message);
